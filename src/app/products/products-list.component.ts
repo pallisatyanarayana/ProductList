627a18cb89a5850033686e46d40154313217fd25
Satyanarayana palli <satyanarayana.palli@indiaapps.com>
2018-11-07 22:07:09 +0530

    first-commit

---
+++ src/app/products/products-list.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-product-list',
+    templateUrl:'./products-list.component.html'
+})
+
+export class ProductListComponent{
+    pageTitle:string = 'Product List';
+    products:any=[
+        {
+            "productid":2,
+            "productName":"Garden Cart",
+            "productCode":"GDN-0023",
+            "releaseDate":"Mar 15,2018",
+            "description":"15 gallon capacity rolling garden",
+            "price":32.99,
+            "starRating":4.2,
+            "imageUrl":"assets/first.png"
+        },
+        {
+            "productid":4,
+            "productName":"Hammer",
+            "productCode":"TBX-0048",
+            "releaseDate":"May 21,2018",
+            "description":"Curved call steel hammer",
+            "price":8.9,
+            "starRating":4.8,
+            "imageUrl":"assets/second.png"
+        }
+    ];
+    
+}
